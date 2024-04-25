ffcdb3581ab9550a329029967136289b11ceea08
Ekaterina696 <katya.osacha@mail.ru>
2024-04-25 16:43:04 +0300

    add todo task and delete

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,8 +1,75 @@
 <template>
-  <q-page class="flex flex-center">
-    <h4>Todo</h4>
+  <q-page class="column">
+    <div class="row q-pa-sm bg-secondary">
+      <q-input @keyup.enter="addTask" filled class="col" square v-model="newTask" placeholder="Добавить задачу" dense>
+        <template v-slot:append>
+          <q-btn @click="addTask" round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
+    <q-list separator bordered>
+      <q-item :class="{ 'done bg-green-1': task.done }" @click="task.done = !task.done" clickable
+        v-for="(task, index) in tasks" :key="task.title" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox class="no-pointer-events" v-model="task.done" val="teal" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn @click.stop="deleteTask(index)" flat round color="primary" icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: [
+        {
+          title: 'tast',
+          done: true,
+        },
+        {
+          title: 'tast2',
+          done: false,
+
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q.dialog({
+        title: 'Внимание!',
+        message: 'Вы действительно хотите удалить эту запись?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index, 1),
+          this.$q.notify('Задача удалена')
+      })
+    },
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false
+      })
+      this.newTask = '';
+    }
+  },
+}
 </script>
+
+<style lang="scss">
+.done {
+.q-item__label {
+  text-decoration: line-through;
+  color: #bbb;
+}
+}
+</style>
